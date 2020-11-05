209f67329d378eee42d9cdd36df1870c57970e08
Martin Jönsson <mr_tines_j@hotmail.com>
2020-11-05 15:06:51 +0100

    Newer, not improved db

--- src/blackhat/Database.java
+++ src/blackhat/Database.java
@@ -1,8 +1,8 @@
 package blackhat;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
     Connection conn = null;
@@ -13,5 +13,40 @@ public class Database {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
+
+    public int getAllUsers() {
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users");
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()) {
+
+            }
+            return
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return ;
+    }
+
+
+
+    public List<Note> getAllNotes(){
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM notes");
+            ResultSet resultSet = stmt.executeQuery();
+            List<Note> notes = new ArrayList<>();
+            while (resultSet.next()) {
+                int noteID = resultSet.getInt("id");
+                String noteHeader = resultSet.getString("header");
+                String noteContent = resultSet.getString("content");
+                int noteOwner = resultSet.getInt("owner");
+                notes.add(new Note(noteID, noteHeader, noteContent, noteOwner));
+            }
+            return notes;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 }
